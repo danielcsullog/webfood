0979abd864ab5cbaab348fcdd9d793e8a1293a8a
Csüllög Dániel <danielcsullog@gmail.com>
2022-06-15 23:45:51 +0200

    fixed e2e tests

--- webfood-nest/src/restaurants/restaurants.module.ts
+++ webfood-nest/src/restaurants/restaurants.module.ts
@@ -3,9 +3,11 @@ import { RestaurantsService } from './restaurants.service';
 import { RestaurantsController } from './restaurants.controller';
 import { Restaurant } from './entities/restaurant';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/entity/user';
 
 @Module({
-  imports: [MikroOrmModule.forFeature({ entities: [Restaurant]})],
+  imports: [MikroOrmModule.forFeature({ entities: [Restaurant, User]})],
   controllers: [RestaurantsController],
   providers: [RestaurantsService]
 })
