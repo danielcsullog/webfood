147c47b294943f4d11d02b2e73b8b499a91110e8
Csüllög Dániel <danielcsullog@gmail.com>
2022-06-17 16:02:01 +0200

    Orders controller & service completed

--- webfood-nest/src/orders/orders.controller.ts
+++ webfood-nest/src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { UserParam } from '../auth/user-param.decorator';
 import { UserDto } from '../users/dto/user.dto';
 import { OrderDto } from './dto/order.dto';
@@ -39,6 +39,39 @@ export class OrdersController {
     ): Promise<OrderDto> {
         const newOrder = await this._orderService.create(orderDto, userDto);
         return new OrderDto(newOrder);
+    }
+
+    @Patch(':id')
+    async update(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updatedOrder: OrderDto,
+        @UserParam() userDto: UserDto
+    ): Promise<OrderDto> {
+        const newOrder = await this._orderService
+            .update(id, updatedOrder, userDto);
+
+        if (!newOrder) {
+            throw new HttpException(
+                'Update failed. Order not found or not updatable!',
+                HttpStatus.FORBIDDEN
+            );
+        }
+
+        return new OrderDto(newOrder);
+    }
+
+    @Delete(':id')
+    async remove(
+        @Param('id', ParseIntPipe) id: number,
+        @UserParam() userDto: UserDto
+    ): Promise<OrderDto> {
+        const deletedOrder = await this._orderService.remove(id, userDto);
+
+        if (!deletedOrder) {
+            throw new HttpException('Remove failed. Order not found or not removable!', HttpStatus.FORBIDDEN);
+        }
+
+        return new OrderDto(deletedOrder);
     }
 
 }
--- webfood-nest/src/orders/orders.service.ts
+++ webfood-nest/src/orders/orders.service.ts
@@ -2,7 +2,7 @@ import { FilterQuery } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityRepository } from '@mikro-orm/sqlite';
 import { Injectable } from '@nestjs/common';
-import { UserDto } from 'src/users/dto/user.dto';
+import { UserDto } from '../users/dto/user.dto';
 import { User, UserRole } from '../users/entity/user';
 import { UserAddress } from '../users/entity/user.address';
 import { Meal } from '../meals/entities/meal';
@@ -12,6 +12,7 @@ import { OrderItem } from './entities/order.item';
 
 @Injectable()
 export class OrdersService {
+
     constructor(
         @InjectRepository(Order)
         private orderRepository: EntityRepository<Order>,
@@ -34,7 +35,7 @@ export class OrdersService {
         }
 
         return await this.orderRepository
-            .find(filters, { populate: ['orderItems.meal', 'orderItems.amount', 'user'] });
+            .find(filters, { populate: ['orderItems', 'user'] });
     }
 
     async findOrderById(orderId: number, user: UserDto): Promise<Order> {
@@ -44,17 +45,17 @@ export class OrdersService {
         }
 
         return await this.orderRepository
-            .findOne(filters, { populate: ['orderItems.meal', 'orderItems.amount', 'user'] });
+            .findOne(filters, { populate: ['orderItems', 'user'] });
     }
 
     async create(orderDto: OrderDto, userDto: UserDto): Promise<Order> {
         const order = new Order();
         order.user = this.userRepository.getReference(userDto.id);
-        order.userAddress = this.userAddressRepository.getReference(orderDto.userAddressId);        
-        order.shortAddress = order.userAddress.city 
-            + " " + 
-            order.userAddress.street 
-            + " " + 
+        order.userAddress = await this.userAddressRepository.findOne(orderDto.userAddressId);
+        order.shortAddress = order.userAddress.city
+            + " " +
+            order.userAddress.street
+            + " " +
             order.userAddress.houseNumber;
         order.orderStatus = OrderStatus.New;
         order.restaurant = orderDto.restaurant;
@@ -70,7 +71,71 @@ export class OrdersService {
         }
 
         await this.orderRepository.persistAndFlush(order);
+        await this.orderRepository.populate(order, ['orderItems', 'user']);
+
+        return order;
+    }
+
+    async update(
+        orderId: number,
+        orderDto: OrderDto,
+        user: UserDto
+    ): Promise<Order> {
+        const filters: FilterQuery<Order> = { orderId };
+
+        if (user.role === UserRole.User) {
+            filters.user = { id: user.id };
+        }
+
+        const order = await this.orderRepository.findOne(filters, {
+            populate: ['orderItems', 'userAddress', 'restaurant']
+        });
+
+
+        if (!order || order.orderStatus !== OrderStatus.New) {
+            return;
+        }
+
+        if (orderDto.userAddressId) {
+            order.userAddress = await this.userAddressRepository
+                .findOne(orderDto.userAddressId) || order.userAddress;
+            console.log(order.userAddress);
+            order.shortAddress = order.userAddress.city + " " +
+                order.userAddress.street + " " +
+                order.userAddress.houseNumber;
+        }
+
+        order.orderStatus = OrderStatus.New;
+
+        if (orderDto.orderItems && orderDto.orderItems.length > 0) {
+            order.orderItems.removeAll();
+            for (const item of orderDto.orderItems) {
+                const newOrderItem = new OrderItem();
+                newOrderItem.order = order;
+                newOrderItem.meal = this.mealRepository.getReference(item.mealId);
+                newOrderItem.amount = item.amount;
+                order.orderItems.add(newOrderItem);
+            }
+        }
+
+        await this.orderRepository.persistAndFlush(order);
         await this.orderRepository.populate(order, ['orderItems', 'user', 'userAddress']);
+
+        return order;
+    }
+
+    async remove(orderId: number, userDto: UserDto): Promise<Order> {
+        const order = await this.orderRepository.findOne({ orderId }, {
+            populate: ['user']
+        });
+        if (!order ||
+            order.orderStatus !== OrderStatus.New ||
+            order.user.id != userDto.id
+        ) {
+            return;
+        }
+
+        await this.orderRepository.removeAndFlush(order);
 
         return order;
     }
