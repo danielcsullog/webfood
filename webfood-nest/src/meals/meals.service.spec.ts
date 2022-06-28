4ae58ffb39e9d584e92a43a77789866a83fe5c38
Csüllög Dániel <danielcsullog@gmail.com>
2022-06-28 19:03:19 +0200

    mealService test providers fixed

--- webfood-nest/src/meals/meals.service.spec.ts
+++ webfood-nest/src/meals/meals.service.spec.ts
@@ -4,6 +4,8 @@ import { RestaurantsService } from '../restaurants/restaurants.service';
 import { Restaurant } from '../restaurants/entities/restaurant';
 import { Meal } from './entities/meal';
 import { MealsService } from './meals.service';
+import { User } from '../users/entity/user';
+import { Order } from '../orders/entities/order';
 
 describe('MealsService', () => {
   let service: MealsService;
@@ -15,6 +17,8 @@ describe('MealsService', () => {
         RestaurantsService,
         { provide: getRepositoryToken(Meal), useValue: {} },
         { provide: getRepositoryToken(Restaurant), useValue: {} },
+        { provide: getRepositoryToken(User), useValue: {} },
+        { provide: getRepositoryToken(Order), useValue: {} },
       ],
     }).compile();
 
