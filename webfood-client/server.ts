8bea229dadb1531e9f58a20ae08efe63108e427a
Csüllög Dániel <danielcsullog@gmail.com>
2022-07-01 18:55:05 +0200

    enchancements

--- webfood-client/server.ts
+++ webfood-client/server.ts
@@ -1,25 +1,24 @@
-
 import * as path from 'path';
 import * as express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
 
-app.use(express.static('./dist/webfood-client'));
-
 app.use(
-  '/api/*',
+  '/api',
   createProxyMiddleware({
+    target: process.env.TARGET || 'http://localhost:3000',
+    changeOrigin: true,
     pathRewrite: {
       '^/api': '',
     },
-    target: process.env.TARGET || 'http://localhost:3000',
-    changeOrigin: true,
   })
 );
 
-/*app.use('*', (req, res) => {
+app.use(express.static('./dist/webfood-client'));
+
+app.use('*', (req, res) => {
   res.sendFile(path.resolve('./dist/webfood-client/index.html'));
-});*/
+});
 
 app.listen(process.env.PORT || 4200);
